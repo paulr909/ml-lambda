88116f1ecb67550260bfb7707048a7ad73f0dde5
paulrogers <paulr909@proton.me>
2023-09-10 10:10:59 +0100

    add src

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Python Version](https://img.shields.io/badge/python-3.10-brightgreen.svg)](https://python.org)
+
+# Machine Learning with Lambda and Serverless Framework
+
+Run your app in a Virtual Environment: http://python-guide-ru.readthedocs.io/en/latest/dev/virtualenvs.html
+
+```shell
+python -m venv venv && source venv/bin/activate
+```
---
+++ handler.py
@@ -0,0 +1,16 @@
+import logging
+
+from sentiment_model import SentimentModel
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
+model = SentimentModel()
+
+
+def handle(event, context):
+    if event.get("source") == "KEEP_LAMBDA_WARM":
+        LOGGER.info("No ML work to do. Just staying warm...")
+        return "Keeping Lambda warm"
+
+    return {"sentiment": model.predict(text=event["text"])}
